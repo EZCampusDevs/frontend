4847563bb2c6c903330a354ea9eb8f4658641627
Alice Nyaa <62451415+Minnowo@users.noreply.github.com>
2023-06-01 22:07:27 -0400

    Update build.sh

--- frontend/build.sh
+++ frontend/build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-docker build -t node_frontend .
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>dockerbuild-frontend-log.out 2>&1
 
+docker build -t node_frontend -f ./Dockerfile .
 
