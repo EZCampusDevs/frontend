1e5cd0b40e6eebefe67ff502a3e674f09eedca46
Alice Nyaa <62451415+Minnowo@users.noreply.github.com>
2023-06-01 22:46:07 -0400

    Update build.sh

--- frontend/build.sh
+++ frontend/build.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+log_dir="/var/log/jenkins-ssh"
+mkdir -p $log_dir
+
 exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
-exec 1>frontend-dockerbuild-log.out 2>&1
+exec 1>"$log_dir/frontend-dockerbuild-log.out" 2>&1
 
 docker build -t node_frontend -f ./Dockerfile .
 
