873e507bf3e954292a602d5747ed3fc1cc18ba3a
Alice Nyaa <62451415+Minnowo@users.noreply.github.com>
2023-06-01 22:44:51 -0400

    Update deploy.sh

--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+log_dir="/var/log/jenkins-ssh"
+mkdir -p $log_dir
+
 exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3
-exec 1>frontend-docker-run-log.out 2>&1
+exec 1>"$log_dir/frontend-dockerrun-log.out" 2>&1
 
 container_name="frontend_prod"
 
