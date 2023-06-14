7c5a6e6fe8c15a08ee8040a0c1119be6869f7f17
jasonmzx <68576119+jasonmzx@users.noreply.github.com>
2023-06-14 12:47:47 -0400

    Init of Google OAuth 2 connection

--- frontend/client/src/pages/IcsPage.js
+++ frontend/client/src/pages/IcsPage.js
@@ -3,13 +3,9 @@ import React from 'react'
 
 //Component imports
 
-//Header
 import PageHeader from '../components/navbar/PageHeader';
-
-
 import TermSelect from '../components/util/TermSelect';
 import CourseEntry from '../components/course_entry/CourseEntry';
-
 
 //Redux
 import { useSelector, useDispatch } from 'react-redux';
@@ -18,16 +14,105 @@ import { assertPush, assertDelete } from '../redux/features/courseEntrySlice';
 //Local 
 import { app_name } from '../util/constant';
 import { Sync_ICS_Post } from '../util/requests';
-
 import '../static/css/main_ui.css';
 
 //Hooks
 import useWindowDimensions from '../util/hooks/useWindowDimensions';
 
-import ENDPOINT from '../util/API';
-
 
 const IcsPage = () => {
+
+//Google Calendar Auth stuff
+  var gapi = window.gapi;
+  console.log(gapi)
+  var CLIENT_ID = "712384750559-gfvgd0eukbv859v4768so8dobj949kf8.apps.googleusercontent.com";
+  var G_API_KEY = "AIzaSyDvkDQ5say67ia8t9ejwpmTddxFN9-w7qA";
+  var DISCOVERY_DOCS = ['https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest'];
+  var SCOPES = "https://www.googleapis.com/auth/calendar.events";
+
+  const handleGoogleCalendar = () => {
+    gapi.load('client:auth2', () => {
+      console.log('loaded client')
+
+      gapi.client.init({
+        apiKey: G_API_KEY,
+        clientId: CLIENT_ID,
+        discoveryDocs: DISCOVERY_DOCS,
+        scope: SCOPES,
+      })
+
+      gapi.client.load('calendar', 'v3', () => console.log('bam!'))
+
+      gapi.auth2.getAuthInstance().signIn()
+      .then(() => {
+
+        console.log("Gets here?")
+        
+        var event = {
+          'summary': 'Awesome Event!',
+          'location': '800 Howard St., San Francisco, CA 94103',
+          'description': 'Really great refreshments',
+          'start': {
+            'dateTime': '2023-06-28T09:00:00-07:00',
+            'timeZone': 'America/Los_Angeles'
+          },
+          'end': {
+            'dateTime': '2023-06-28T17:00:00-07:00',
+            'timeZone': 'America/Los_Angeles'
+          },
+          'recurrence': [
+            'RRULE:FREQ=DAILY;COUNT=2'
+          ],
+          'attendees': [
+            {'email': 'lpage@example.com'},
+            {'email': 'sbrin@example.com'}
+          ],
+          'reminders': {
+            'useDefault': false,
+            'overrides': [
+              {'method': 'email', 'minutes': 24 * 60},
+              {'method': 'popup', 'minutes': 10}
+            ]
+          }
+        }
+
+        var request = gapi.client.calendar.events.insert({
+          'calendarId': 'primary',
+          'resource': event,
+        })
+
+        console.log("Inserted?")
+
+        request.execute(event => {
+          console.log(event)
+          window.open(event.htmlLink)
+        })
+        
+
+        /*
+            Uncomment the following block to get events
+        */
+        /*
+        // get events
+        gapi.client.calendar.events.list({
+          'calendarId': 'primary',
+          'timeMin': (new Date()).toISOString(),
+          'showDeleted': false,
+          'singleEvents': true,
+          'maxResults': 10,
+          'orderBy': 'startTime'
+        }).then(response => {
+          const events = response.result.items
+          console.log('EVENTS: ', events)
+        })
+        */
+    
+
+      })
+    })
+  }
+
+
   document.title = app_name + " | Event Planner Heatmap"
 
   //Redux state
@@ -152,6 +237,8 @@ const IcsPage = () => {
         <span className="error_text r_font"> {errMsg} </span>
         
         {RenderLink()}<br/><br/>
+
+        <button onClick={() => {handleGoogleCalendar()}}>Google OAuth</button>
 
       </div>
 
