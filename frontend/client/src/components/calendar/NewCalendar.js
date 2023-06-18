51aee15c480e83c537ad81acefe9e7d6b0b841fb
jasonmzx <68576119+jasonmzx@users.noreply.github.com>
2023-06-18 00:11:56 -0400

    Decent understand of tailwind col/row system and a basic impl

--- frontend/client/src/components/calendar/NewCalendar.js
+++ frontend/client/src/components/calendar/NewCalendar.js
@@ -4,33 +4,78 @@ import React from 'react'
 
 import { CalendarPlaceholder } from '../../util/requests'
 import { cParse2 } from '../../util/calendarJSON';
+import NewCalendarEvent from './NewCalendarEvent';
 
 const NewCalendar = () => {
 
+    //const eventsOverlay = [<NewCalendarEvent/>];
+    
+    
     // TODO: try to use rrule with it
 
+
     const generateCalendar = () => {
+
+
         const weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
         
-        let headers = [];
+        let rows = [];
 
-        const timeslots = 48;
+        const earliest = 0;
+        const latest   = 48;
 
 
         //Generate Calendar Header
-
+        let header = [];
         for(const [wI,wD] of weekdays.entries()) {
-           let Header = (<div className="text-center font-bold">
+           let headerEntry = (<div className="text-center font-bold">
                         {weekdays[wI]}
                         </div>);
-            headers.push(Header);
+
+        rows.push(headerEntry);
+
         }
 
 
+        let slotTimes = [];
+        for (let i = earliest; i < latest; i++) {
+            const hour = Math.floor(i / 2);
+            const minutes = (i % 2) === 0 ? '00' : '30';
+            const time = `${hour}:${minutes}`;
+              
+            const timeSlot = ( <div key={i} className="h-4 text-center col-start-1 col-span-1">
+                    {time}
+                  </div> );
+            slotTimes.push(timeSlot); 
+            
 
-        let cols = <div className="grid grid-cols-7 gap-2">
-            {headers}
-        </div>
+        }
+        
+
+        let cols = (
+            <div className="grid grid-cols-8 gap-1">
+              {/* TIME SLOTS (col 1) */}
+              <div className="row-span-49 col-span-1">
+                {/* SWITCH Button, TODO: add styling and functionality */}
+                <span>Switch</span>
+                {slotTimes}
+              </div>
+          
+              {/* Starting from second column */}
+              <div className="grid col-span-7 grid-cols-7 gr-50">
+
+                <div className=" bg-blue-200 mt-6 h-16">01</div>
+                <div className=" col-start-1
+                bg-green-200 mt-20 h-20
+                ">
+
+                </div>
+
+
+              </div>
+
+            </div>
+          );
 
         return cols;
     }
---
+++ frontend/client/src/components/calendar/NewCalendarEvent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const NewCalendarEvent = ({dayOfWeek, timeOfDay}) => {
+
+    
+
+  return (
+    <div className="h-10 bg-red-500 top-10 mt-20 col-start-2 col-span-1">
+LLL...
+    </div>
+  )
+}
+
+export default NewCalendarEvent
