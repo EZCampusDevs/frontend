43510d8e0a2297c5ef292ce2c1a7c4636f1e26d9
jasonmzx <68576119+jasonmzx@users.noreply.github.com>
2023-06-20 13:54:12 -0400

    GPT 4's first commit

--- frontend/client/src/pages/SchoolRoutePage.js
+++ frontend/client/src/pages/SchoolRoutePage.js
@@ -1,25 +1,43 @@
-import React from 'react'
-import PageHeader from '../components/navbar/PageHeader'
+import React from 'react';
+import PageHeader from '../components/navbar/PageHeader';
 
-
-//Try to re-use styles from index.css ! (Going for UI Uniformity where all components look the same across all pages)
+const schools = [
+  'otu',
+  'uvic',
+  'dc',
+  'School 4',
+  'School 5',
+  'School 6',
+  'School 7',
+  'School 8',
+  'School 9',
+  'School 10',
+];
 
 const SchoolRoutePage = () => {
-
-
-    const genBtn = () => {
-        return <button onClick={() => {window.location.href = "https://otu.ezcampus.org"}}> Click </button>
-    }
-
+  const goToSchoolPage = (schoolName) => {
+    window.location.href = `https://${schoolName}.ezcampus.org`;
+  };
 
   return (
-    <div>
-    <PageHeader/>
+    <>
+    <PageHeader />
+    <div className="flex flex-col items-center justify-center min-h-screen bg-white dark:bg-gray-800">
 
-    TODO: Make this page {genBtn()}
-
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-4 p-4 max-w-screen-xl w-full">
+        {schools.map((school, index) => (
+          <button
+            key={index}
+            className="flex flex-col items-center justify-center p-6 bg-blue-500 hover:bg-blue-600 transition duration-200 ease-in-out text-white rounded-lg shadow-md dark:text-gray-800 dark:bg-gray-100 dark:hover:bg-gray-200"
+            onClick={() => goToSchoolPage(school)}
+          >
+            {school}
+          </button>
+        ))}
+      </div>
     </div>
-  )
-}
+    </>
+  );
+};
 
-export default SchoolRoutePage
+export default SchoolRoutePage;
