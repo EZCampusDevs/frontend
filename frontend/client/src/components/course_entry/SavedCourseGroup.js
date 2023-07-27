864d3f772b5c0fa05bf9345e5c73bd7295b93c0b
jasonmzx <68576119+jasonmzx@users.noreply.github.com>
2023-07-26 23:28:28 -0400

    ICS Page polish part. 1

--- frontend/client/src/components/course_entry/SavedCourseGroup.js
+++ frontend/client/src/components/course_entry/SavedCourseGroup.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { classTypeIcon } from '../../util/reactHelper';
+import { classTypeIcon, locIconBuilder, profIcon } from '../../util/reactHelper';
 
 
 const SavedCourseGroup = ({Gindex, courseCode, entries, ReduxDeleteCourse, iconBuilder}) => {
@@ -25,29 +25,51 @@ const SavedCourseGroup = ({Gindex, courseCode, entries, ReduxDeleteCourse, iconB
     {/* ENTRY MAPPING WITHIN GROUP */}
     
     {entries.map((entry, index) => (
-        <div className={"saved_entry bg-blue-200"} key={index}>
-          <div className="grid grid-cols-1 md:grid-cols-6 gap-4">
+    <div className="saved_entry bg-blue-200 dark:bg-gray-800 p-4 rounded-lg shadow-md my-2 text-gray-900 dark:text-white" key={index}>
+        <div className="grid grid-cols-1 md:grid-cols-6 gap-4">
             {/* COL 1 */}
             <div className="md:col-start-1 r_font">
-              <span className="sub_title">{entry.course_code}</span><br></br>
-              {classTypeIcon(entry.class_type)}
+                <div className="font-bold text-lg">{entry.course_code}</div>
+                {classTypeIcon(entry.class_type)}
             </div>
-  
+
             {/* COL 2 */}
             <div className="md:col-start-2 r_font"> 
-              <span className="font-bold">CRN:</span> {entry.course_crn}
+                <span className="font-bold">CRN:</span> {entry.course_crn}
             </div>  
-  
+
             {/* COL 3 */}
-            <div className="md:col-start-3 r_font">{entry.course_title}</div>                
-  
+            <div className="md:col-start-3 r_font">
+                <div className="font-bold">{entry.course_title}</div>
+
+                <div>{entry.course_desc}</div>
+            </div>                
+
+            {/* COL 4 */}
+            <div className="md:col-start-4 r_font">
+                {entry.instructors.map((instructor, idx) => (
+                    
+                    <span  key={idx}>
+                    {profIcon(instructor.instructor_name)}
+                    </span>
+                ))}
+            </div>
+
+            {/* COL 5 */}
+            <div className="md:col-start-5 r_font">
+
+                {locIconBuilder(entry.extra[0].building)}
+
+                {entry.extra[0].building_description}
+            </div>
+
             {/* COL 6 */}
-            <button className="md:col-start-6 large_red_btn" onClick={() => {ReduxDeleteCourse(entry.course_data_id)}}>
-              Remove
-            </button>
-          </div>
+                <button className="md:col-start-6 large_red_btn h-16" onClick={() => {ReduxDeleteCourse(entry.course_data_id)}}>
+                    Remove
+                </button>
         </div>
-      ))}
+    </div>
+))}
     </div>
 
     :
--- frontend/client/src/pages/IcsPage.js
+++ frontend/client/src/pages/IcsPage.js
@@ -20,6 +20,7 @@ import '../static/css/main_ui.css';
 import useWindowDimensions from '../util/hooks/useWindowDimensions';
 import SchoolRouteRenderWall from '../components/schools/SchoolRouteRenderWall';
 
+const googleSvg = <svg xmlns="http://www.w3.org/2000/svg" className={`h-6 w-6 mr-3 text-gray-900 dark:text-white`} viewBox="0 0 48 48"><path id="a" d="M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z"/></svg>
 
 const IcsPage = () => {
 
@@ -78,7 +79,7 @@ const IcsPage = () => {
         return (
         <>
           <br/>
-          <p>Our server(s) are currently processing your request ... </p>
+          <p className="sub_title">Our server(s) are currently processing your request ... </p>
           </>
         )
     }
@@ -88,12 +89,13 @@ const IcsPage = () => {
       return (
         <>
           <br/>
-          <p>Successfully created your .ics file </p>
+          <p className="sub_title">Successfully created your .ics file!</p>
           <br/>
-          <a className="large-blue-btn" href={blobURL} download="calendar.ics">CLICK TO DOWNLOAD ({blobSize} bytes)</a>
+      <a className="large_blue_btn" href={blobURL} download="calendar.ics"> Download `calendar.ics` | ({blobSize} bytes)</a>
           <br/><br/>
-          <button className="" onClick={() => {handleSubmitICS()}}>
-        Re-Generate My Calendar File
+      
+      <button className="large_warning_btn" onClick={() => {handleSubmitICS()}}>
+      Re-Generate My Calendar File
       </button>
         </>);
     } else {
@@ -104,8 +106,17 @@ const IcsPage = () => {
         <button className="large_blue_btn" onClick={() => {handleSubmitICS()}}>
           Generate My Calendar file 
         </button>
+
+        <br/><br/>
+
+        <button className="large_red_btn flex items-center justify-center space-x-2" onClick={() => {handleGoogleCalendar()}}>
+          {googleSvg}
+          Export to Google Calendar
+        </button>
       </>);
-      } else {
+      } 
+      
+      else {
         return (<><br/>
         <button className="large_blue_btn disabled">
           Select courses first...
@@ -116,23 +127,6 @@ const IcsPage = () => {
     }
     
 }
-
-  //
-
-  React.useEffect( () => {
-  }, [saved_entries])
-
-
-//! Might be removed if we remove the video embd
-  //Based on the Width of the viewport, this function will return a fair height/width to match the size of viewport.
-  const iframeViewportScale = (axis, width) => {
-    if(axis) { //If width
-      return MOBILE_SWITCH > width ? "200" : "430";
-    } 
-    if(!axis) { //If Height
-      return MOBILE_SWITCH > width ? "150" : "315";
-    }
-  }
 
   return (
 
@@ -177,9 +171,6 @@ const IcsPage = () => {
         {/* <a href="https://script.google.com/macros/s/AKfycbwuVrj36fmE9noinxa3OE52zttxa5uQkxRpB8BkjHtX9TELzYGZitY2pWras9wyz3hoxg/exec?title=EZCAMPUS EVENT">
               Noice
         </a> */}
-
-        <button className="large_blue_btn" onClick={() => {handleGoogleCalendar()}}>Save to Google Calendar (Coming Soon)</button>
-
       </div>
 
       </div>
