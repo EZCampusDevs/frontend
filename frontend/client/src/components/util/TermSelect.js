5ff47b649637fe6603d3d8178fc52fd875e24cf9
Daniel Jeon <48095779+danielljeon@users.noreply.github.com>
2023-08-02 00:09:37 -0400

    Minor TermSelect.js cleanup

--- frontend/client/src/components/util/TermSelect.js
+++ frontend/client/src/components/util/TermSelect.js
@@ -1,48 +1,48 @@
 import React from 'react'
 
 //Redux 
-import { useSelector, useDispatch } from 'react-redux';
-import { setParameter } from '../../redux/features/configSelectSlice';
-import { SchoolTermRequest } from '../../util/requests';
+import {useSelector, useDispatch} from 'react-redux';
+import {setParameter} from '../../redux/features/configSelectSlice';
+import {SchoolTermRequest} from '../../util/requests';
 
 const TermSelect = () => {
 
-    //Redux String State Variables:
-    const selectedSchool = useSelector((state) => state.configSelect.selected_school);
+  //Redux String State Variables:
+  const selectedSchool = useSelector((state) => state.configSelect.selected_school);
 
-    //Redux Array<Objects> State Variables:
-    const terms = useSelector((state) => state.configSelect.terms);    
+  //Redux Array<Objects> State Variables:
+  const terms = useSelector((state) => state.configSelect.terms);
 
-    //Redux Dispatchers 
+  //Redux Dispatchers
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    const setReduxParameter = (identifier, value, type) => {
-        dispatch(setParameter({
-          "identifier" : identifier,
-          "value" : value
-        }));
-      }
-    
-    
-    //Term Options Builder
-
-    //TODO: Improve the building of terms, CSS and UI improvemnts
-
-    const buildTerms = (termsArray) => {
-      let dump = [];
-
-      for(const entry of termsArray){
-        dump.unshift( <option value={entry.termId}>{entry.termDescription}</option> );
-      }
-
-      return dump;
-    }  
+  const setReduxParameter = (identifier, value, type) => {
+    dispatch(setParameter({
+      "identifier": identifier,
+      "value": value
+    }));
+  }
 
 
-    //React UseEffect statements:
+  //Term Options Builder
 
-    React.useEffect( //On MOUNT
+  //TODO: Improve the building of terms, CSS and UI improvemnts
+
+  const buildTerms = (termsArray) => {
+    let dump = [];
+
+    for (const entry of termsArray) {
+      dump.unshift(<option value={entry.termId}>{entry.termDescription}</option>);
+    }
+
+    return dump;
+  }
+
+
+  //React UseEffect statements:
+
+  React.useEffect( //On MOUNT
     () => {
 
       let schoolUniqueName = null;
@@ -54,43 +54,43 @@ const TermSelect = () => {
       }
 
 
-        SchoolTermRequest(
-          "terms", //Redux Identifier for configSelect slice
-          { "school_name" : schoolUniqueName, "school_id" : null }, //API Request POST body
-          setReduxParameter //callback
-        );
-    }, [] );
-  
-    //Reset Default Term upon change of Terms list
-    React.useEffect(
-      () => {
-        //If terms list isn't empty, select the latest term as default selection
+      SchoolTermRequest(
+        "terms", //Redux Identifier for configSelect slice
+        {"school_name": schoolUniqueName, "school_id": null}, //API Request POST body
+        setReduxParameter //callback
+      );
+    }, []);
 
-        if(terms.length){
-          setReduxParameter("selected_term", terms[terms.length-1].termId, false);
-        }
-      }, [terms]
-    );
+  //Reset Default Term upon change of Terms list
+  React.useEffect(
+    () => {
+      //If terms list isn't empty, select the latest term as default selection
 
-    //! RETURN FN.
-return(<> 
-    <div className="flex items-center">
-        <span className="sub_title"> Select Term for <span className="font-semibold">{selectedSchool} :</span> </span>   
-    </div>   
+      if (terms.length) {
+        setReduxParameter("selected_term", terms[terms.length - 1].termId, false);
+      }
+    }, [terms]
+  );
 
-    <select
+  //! RETURN FN.
+  return (<>
+      <div className="flex items-center">
+        <span className="sub_title"> Select Term for <span className="font-semibold">{selectedSchool}:</span> </span>
+      </div>
+
+      <select
         class="large_select_field"
         // defaultValue={configs[0]}
         onClick={e => {
-        setReduxParameter("selected_term", e.target.value, false)}}
-    >
+          setReduxParameter("selected_term", e.target.value, false)
+        }}
+      >
         {buildTerms(terms)}
-    </select>
+      </select>
     </>
-); //! END OF RETURN
+  ); //! END OF RETURN
 
 }
-
 
 
 export default TermSelect
