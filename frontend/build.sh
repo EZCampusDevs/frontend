287b067eab775dd78dd377f0565ff515281a08ed
Minnowo <62451415+Minnowo@users.noreply.github.com>
2023-08-12 21:47:09 -0400

    updated build and deploy pipeline scripts

--- frontend/build.sh
+++ frontend/build.sh
@@ -1,14 +1,45 @@
 #!/bin/sh
 
-log_dir="$HOME/log/jenkins-ssh"
-mkdir -p $log_dir
 
-log_file="$log_dir/frontend-dockerbuild-log.out"
-touch log_file
+USE_LOG_FILE_ARGUMENT="USE_LOG_FILE"
 
-exec 3>&1 4>&2
-trap 'exec 2>&4 1>&3' 0 1 2 3
-exec 1>$log_file 2>&1
+if [ $# != 0 ]; then
 
-docker build -t node_frontend -f ./Dockerfile .
+   echo "Processing start arguments"
+
+   for arg in "$@"; do
+
+      if [ "$arg" = "$USE_LOG_FILE_ARGUMENT" ]; then
+
+         echo "Writing all stdout and stderr to file"
+
+         log_dir="$HOME/log/jenkins-ssh"
+         mkdir -p $log_dir
+
+         log_file="$log_dir/run-datascraper-log.out"
+         touch $log_file
+
+         exec 3>&1 4>&2
+         trap 'exec 2>&4 1>&3' 0 1 2 3
+         exec 1>$log_file 2>&1
+
+      fi
+
+   done
+
+fi
+
+
+if [ ! -d ./client ]; then
+
+   echo './client directory does not exist, this contains the source code of the frontend!'
+   exit 1
+
+fi
+
+cd ./client
+
+IMAGE_NAME="react_build_prod"
+
+docker build -t "$IMAGE_NAME" -f ./Dockerfile .
 
--- frontend/deploy.sh
+++ frontend/deploy.sh
@@ -1,28 +1,49 @@
 #!/bin/sh
 
-log_dir="$HOME/log/jenkins-ssh"
-mkdir -p $log_dir
 
-log_file="$log_dir/frontend-dockerrun-log.out"
-touch log_file
+USE_LOG_FILE_ARGUMENT="USE_LOG_FILE"
 
-exec 3>&1 4>&2
-trap 'exec 2>&4 1>&3' 0 1 2 3
-exec 1>$log_file 2>&1
+if [ $# != 0 ]; then
 
-container_name="frontend_prod"
+   echo "Processing start arguments"
+
+   for arg in "$@"; do
+
+      if [ "$arg" = "$USE_LOG_FILE_ARGUMENT" ]; then
+
+         echo "Writing all stdout and stderr to file"
+
+         log_dir="$HOME/log/jenkins-ssh"
+         mkdir -p $log_dir
+
+         log_file="$log_dir/deploy-frontend-log.out"
+         touch $log_file
+
+         exec 3>&1 4>&2
+         trap 'exec 2>&4 1>&3' 0 1 2 3
+         exec 1>$log_file 2>&1
+
+      fi
+
+   done
+
+fi
+
+
+IMAGE_NAME="react_build_prod"
+CONTAINER_NAME="frontend_prod"
 
 echo "Stopping container..."
 
-docker stop $container_name || true
+docker stop $CONTAINER_NAME || true
 
-while [ "$(docker inspect -f '{{.State.Running}}' "$container_name" 2>/dev/null)" = "true" ]; do
+while [ "$(docker inspect -f '{{.State.Running}}' "$CONTAINER_NAME" 2>/dev/null)" = "true" ]; do
     echo "Waiting for container to stop..."
     sleep 1
 done
 
 echo "Running build..."
 
-docker run -itd --rm -p 3000:3000 --network EZnet --name $container_name node_frontend
+docker run -itd --rm -p 3000:80 --network EZnet --name "$CONTAINER_NAME" "$IMAGE_NAME"
 
 echo "Deploy done."
